7f89a5797d89c43c7ff70356661bfa76b06d202d
Denis <emtsev.denis@yandex.ru>
2016-10-03 16:07:07 +0400

    Added entry point

---
+++ RPIS41/EMTSEV/wdad/Application.java
@@ -0,0 +1,10 @@
+package RPIS41.EMTSEV.wdad;
+
+/**
+ * Created by U_LIVT35416 on 03.10.2016.
+ */
+public class Application {
+    public static void main(String[] args){
+        System.out.println(" I'm Emtsev Denis, and I'm not a monkey");
+    }
+}
